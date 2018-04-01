578c5df23014418c72dc6d7a79d045bee1fbb396
Igor <verycooleagle@gmail.com>
2018-04-01 23:49:06 +0300

    Maybe close 2.

--- index-app.js
+++ index-app.js
@@ -3,45 +3,22 @@ const app           = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menu          = electron.Menu;
 const server        = require('./server');
-
 const includes = async () => {
-
 	try {
-		//~ Dev setting
-
-		const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
-
-		await installExtension(REACT_DEVELOPER_TOOLS);
-		await installExtension(REDUX_DEVTOOLS);
-
-		//~ End dev setting
-
 		let mainWindow = new BrowserWindow({
 			width  : 400,
 			height : 600,
 		});
-
 		await server.run(mainWindow);
-
-		//~ Dev setting
-
-		mainWindow.maximize();
-		mainWindow.toggleDevTools();
-
-		//~ End dev setting
-
+		
 		mainWindow.loadURL(`file://${__dirname}/html/index.html`);
-
 		mainWindow.on('closed', () => {
 			mainWindow = null;
 			app.quit();
 		});
-
 		require('./menu-app').add(Menu, app);
-
 	} catch (err) {
 		console.log('Error: ', err);
 	}
 };
-
 app.on('ready', includes);
