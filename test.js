414fb1131598e1c89e483a07b92d2284131eb620
Igor <verycooleagle@gmail.com>
2018-04-02 00:08:20 +0300

    Close #2. Work in window 10.

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 const SudoExec       = require('./libs/sudo-promt-promise');
-const sudoExec       = new SudoExec();/**
- * Created by Igor on 01.04.2018.
- */
+const sudoExec       = new SudoExec();
+
 const cmd     = require('node-cmd');
 
 cmd.get('docker ps',function(err, data, stderr) {
