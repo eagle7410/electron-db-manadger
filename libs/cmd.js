39e137656a92facd5ee721ddafe08fba950a8d4f
Igor <verycooleagle@gmail.com>
2018-04-03 22:34:42 +0300

    Close #1. Work in mackbook.
    * for run project in mac
    
    * Close #1. Work in macbook.

--- libs/cmd.js
+++ libs/cmd.js
@@ -1,16 +1,22 @@
 let cmd = require('node-cmd');
+const os = require('os');
 
 class Cmd {
-	constructor () {
-		this.cmd = cmd;
-	}
-	setPassword (password) {
-		return this;
-	}
+    constructor () {
+        this.cmd = cmd;
+    }
+    setPassword (password) {
+        return this;
+    }
 
-	exec (arCmd, call) {
-		this.cmd.get(arCmd.join(' '), (err, data) => call(err, null, data));
-	}
+    exec (arCmd, call) {
+        let cmd =arCmd.join(' ');
+
+        if (['darwin'].includes(os.platform()) )
+            cmd = cmd.replace('docker', '/Applications/Docker.app/Contents/Resources/bin/docker');
+
+        this.cmd.get(cmd, (err, data) => call(err, null, data));
+    }
 }
 
 module.exports = new Cmd();
